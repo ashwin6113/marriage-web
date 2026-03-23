095a3241879995686978d0715a87b745a94babab
Ashwin John <ashwinjohn611@gmail.com>
2026-03-23 23:56:49 +0530

    pipeline fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,18 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import nextVitals from "eslint-config-next/core-web-vitals.js";
+import nextTs from "eslint-config-next/typescript.js";
 
-const eslintConfig = defineConfig([
+const eslintConfig = defineConfig({
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
+  ignores: [
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
     "next/core-web-vitals",
     "prettier"
-  ]),
-]);
+  ],
+});
 
 export default eslintConfig;
